4470de2ea114105042c88229ef6bb72060360d50
abu04 <50547423+abu04@users.noreply.github.com>
2021-04-18 13:49:50 +0530

    Update ContractService.java

--- fms/src/main/java/com/cg/fms/service/ContractService.java
+++ fms/src/main/java/com/cg/fms/service/ContractService.java
@@ -1,5 +1,13 @@
 package com.cg.fms.service;
+import java.util.List;
+
+import com.cg.fms.dto.Contract;
 
 public interface ContractService {
+  public Contract getContract(int contractno);
+	public List<Contract> getAllContracts();
+	public boolean addContract(Contract contract);
+	public boolean updateContract(Contract contract);
+	public boolean deteteContract(int contractno);
 
 }
