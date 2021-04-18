c1ba9fb4e2e61a022e64e21bcfd02a1c01301774
abu04 <50547423+abu04@users.noreply.github.com>
2021-04-18 13:50:57 +0530

    Update ContractServiceImpl.java

--- fms/src/main/java/com/cg/fms/service/ContractServiceImpl.java
+++ fms/src/main/java/com/cg/fms/service/ContractServiceImpl.java
@@ -1,5 +1,38 @@
 package com.cg.fms.service;
+import java.util.List;
+
+import com.cg.fms.dto.Contract;
 
 public class ContractServiceImpl implements ContractService {
+  @Override
+	public Contract getContract(int contractno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Contract> getAllContracts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addContract(Contract contract) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateContract(Contract contract) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deteteContract(int contractno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 
 }
