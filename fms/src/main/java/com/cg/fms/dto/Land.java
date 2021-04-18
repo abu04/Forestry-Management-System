6319c1cc1c1258a79fd75832269e694e1f443b84
abu04 <50547423+abu04@users.noreply.github.com>
2021-04-18 13:23:29 +0530

    Update Land.java

--- fms/src/main/java/com/cg/fms/dto/Land.java
+++ fms/src/main/java/com/cg/fms/dto/Land.java
@@ -1,5 +1,41 @@
 package com.cg.fms.dto;
 
 public class Land {
-
+  private int landid;
+	private String surveyno;
+	private String ownername;
+	private String landArea;
+	
+	
+	public Land(int landid, String surveyno, String ownername, String landArea) {
+		this.landid = landid;
+		this.surveyno = surveyno;
+		this.ownername = ownername;
+		this.landArea = landArea;
+	}
+	public int getLandid() {
+		return landid;
+	}
+	public void setLandid(int landid) {
+		this.landid = landid;
+	}
+	public String getSurveyno() {
+		return surveyno;
+	}
+	public void setSurveyno(String surveyno) {
+		this.surveyno = surveyno;
+	}
+	public String getOwnername() {
+		return ownername;
+	}
+	public void setOwnername(String ownername) {
+		this.ownername = ownername;
+	}
+	public String getLandArea() {
+		return landArea;
+	}
+	public void setLandArea(String landArea) {
+		this.landArea = landArea;
+	}
+	
 }
