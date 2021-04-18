72adde99dfd5526c9ef18dd04b2baa684b63a98a
abu04 <50547423+abu04@users.noreply.github.com>
2021-04-18 13:46:12 +0530

    Update ILandDao.java

--- fms/src/main/java/com/cg/fms/dao/ILandDao.java
+++ fms/src/main/java/com/cg/fms/dao/ILandDao.java
@@ -1,5 +1,13 @@
 package com.cg.fms.dao;
+import java.util.List;
+
+import com.cg.fms.dto.Land;
 
 public interface ILandDao {
+  public Land getLand(String surveyno);
+	public List<Land> getAllLands();
+	public boolean addLand(Land Land);
+	public boolean updateLand(Land land );
+	public boolean deleteLand(String surveyno);
 
 }
