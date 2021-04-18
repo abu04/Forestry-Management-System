0406f665d25d527cbb75b0fd6e32bb2a28948744
abu04 <50547423+abu04@users.noreply.github.com>
2021-04-18 13:48:52 +0530

    Update ContractDaoImpl.java

--- fms/src/main/java/com/cg/fms/dao/ContractDaoImpl.java
+++ fms/src/main/java/com/cg/fms/dao/ContractDaoImpl.java
@@ -1,5 +1,37 @@
 package com.cg.fms.dao;
+import java.util.List;
+
+import com.cg.fms.dto.Contract;
 
 public class ContractDaoImpl implements ContractDao {
+  @Override
+	public Contract getContract(int contractno) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Contract> getAllContracts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addContract(Contract contract) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateContract(Contract contract) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deteteContract(int contractno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
