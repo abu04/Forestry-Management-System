2f98d7ca5f6c03be6fdf330a5197b0057aac884b
abu04 <50547423+abu04@users.noreply.github.com>
2021-04-18 13:51:40 +0530

    Update LandService.java

--- fms/src/main/java/com/cg/fms/service/LandService.java
+++ fms/src/main/java/com/cg/fms/service/LandService.java
@@ -1,5 +1,13 @@
 package com.cg.fms.service;
+import java.util.List;
+
+import com.cg.fms.dto.Land;
 
 public interface LandService {
+  public Land getLand(String surveyno);
+	public List<Land> getAllLands();
+	public boolean addLand(Land Land);
+	public boolean updateLand(Land land );
+	public boolean deleteLand(String surveyno);
 
 }
