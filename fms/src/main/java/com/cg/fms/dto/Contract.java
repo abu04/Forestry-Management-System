0ca1e1a41c469c3cd2d3bd1bac05f9cdbb002b11
abu04 <50547423+abu04@users.noreply.github.com>
2021-04-18 13:26:41 +0530

    Update Contract.java

--- fms/src/main/java/com/cg/fms/dto/Contract.java
+++ fms/src/main/java/com/cg/fms/dto/Contract.java
@@ -1,5 +1,41 @@
 package com.cg.fms.dto;
 
 public class Contract {
+  private int contractno;
+	//private String deliveryplace;
+	private String expirydate;
+	private String quantity;
+	private Land land;
+  
+	public Contract(int contractno, String expirydate, String quantity, Land land) {
+		this.contractno = contractno;
+		this.expirydate = expirydate;
+		this.quantity = quantity;
+		this.land = land;
+	}
+	public int getContractno() {
+		return contractno;
+	}
+	public void setContractno(int contractno) {
+		this.contractno = contractno;
+	}
+	public String getExpirydate() {
+		return expirydate;
+	}
+	public void setExpirydate(String expirydate) {
+		this.expirydate = expirydate;
+	}
+	public String getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+	public Land getLand() {
+		return land;
+	}
+	public void setLand(Land land) {
+		this.land = land;
+	}
 
 }
