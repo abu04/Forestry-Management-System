dc4bc73ac7243dd1824e4055575ffa2a1053c0b8
abu04 <50547423+abu04@users.noreply.github.com>
2021-04-18 20:12:03 +0530

    Update ContractDao.java

--- fms/src/main/java/com/cg/fms/dao/ContractDao.java
+++ fms/src/main/java/com/cg/fms/dao/ContractDao.java
@@ -1,5 +1,16 @@
 package com.cg.fms.dao;
 
-public interface ContractDao {
+import java.util.List;
+
+import com.cg.fms.dto.Contract;
+
+public interface ContractDAO {
+	public Contract getContract(int contractno);
+	public List<Contract> getAllContracts();
+	public boolean addContract(Contract contract);
+	public boolean updateContract(Contract contract);
+	public boolean deleteContract(int contractno);
+	public void beginTransaction();
+	public void commitTransaction();
 
 }
