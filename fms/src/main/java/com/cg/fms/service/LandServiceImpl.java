de18aa2b4faab62f435957d9446a4f83041eb673
abu04 <50547423+abu04@users.noreply.github.com>
2021-04-18 13:52:29 +0530

    Update LandServiceImpl.java

--- fms/src/main/java/com/cg/fms/service/LandServiceImpl.java
+++ fms/src/main/java/com/cg/fms/service/LandServiceImpl.java
@@ -1,5 +1,37 @@
 package com.cg.fms.service;
+import java.util.List;
+
+import com.cg.fms.dto.Land;
 
 public class LandServiceImpl implements LandService {
+  @Override
+	public Land getLand(String surveyno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Land> getAllLands() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addLand(Land Land) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateLand(Land land) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteLand(String surveyno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
